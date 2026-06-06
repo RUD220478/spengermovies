cc3a57d6cd57c5758cb77a44dd0064d485a3be8c
Mladen Rudic <RUD220478@spengergasse.at>
2026-06-06 21:22:11 +0200

    Completed 03_Layout, added cards

--- src/main/java/at/spengergasse/views/show/ShowView.java
+++ src/main/java/at/spengergasse/views/show/ShowView.java
@@ -1,11 +1,20 @@
 package at.spengergasse.views.show;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Flex;
+import com.vaadin.flow.theme.lumo.LumoUtility.Margin.Minus.Vertical;
+
 import at.spengergasse.views.home.HomeView;
+
+import java.util.OptionalDouble;
+
 import org.vaadin.lineawesome.LineAwesomeIconUrl;
 
 @PageTitle("Show")
@@ -19,29 +28,46 @@ public class ShowView extends VerticalLayout {
         add(HomeView.getHeader());
 
         H2 title = new H2("Vorführungen");
+        add(title);
 
-        H2 movie1 = new H2("Avengers");
-        Paragraph price1 = new Paragraph("Pro Person: 10,00 Euro");
-        Paragraph private1 = new Paragraph("Privatvorstellung ab 200 Euro");
-        Paragraph gruppe1 = new Paragraph("Gruppenermäßigung 25 % ab 10 Personen");
+        Component card0 = getCard("Avengers", 10.50, OptionalDouble.of(20.00));
+        Component card1 = getCard("Batman", 8.90, OptionalDouble.of(14.00));
+        Component card2 = getCard("Matrix", 6.50, OptionalDouble.of(12.00));
+        Component card3 = getCard("Lost in Translation", 0.00, OptionalDouble.empty());
+        Component card4 = getCard("Harry Potter", 23.00, OptionalDouble.empty());
 
-        H2 movie2 = new H2("Batman");
-        Paragraph price2 = new Paragraph("Pro Person: 8,00 Euro");
-        Paragraph private2 = new Paragraph("Privatvorstellung ab 160 Euro");
-        Paragraph gruppe2 = new Paragraph("Gruppenermäßigung 30 % ab 20 Personen");
+        FlexLayout cards = new FlexLayout(card0, card1, card2, card3, card4);
+        cards.setWidthFull();
+        cards.setJustifyContentMode(JustifyContentMode.CENTER);
+        cards.setFlexWrap(FlexLayout.FlexWrap.WRAP);
+        add(cards);
 
-        H2 movie3 = new H2("Matrix");
-        Paragraph price3 = new Paragraph("Pro Person: 6,50 Euro");
-        Paragraph private3 = new Paragraph("Privatvorstellung ab 120 Euro");
-        Paragraph gruppe3 = new Paragraph("Gruppenermäßigung 10 % ab 30 Personen");
+        Paragraph info = new Paragraph("Wir freuen uns auf Ihren Besuch!");
+        info.setWidth("100%");
+        info.getStyle().set("text-align", "center");
+        add(info);
+    }
 
-        add(
-                title,
-                movie1, price1, private1, gruppe1,
-                movie2, price2, private2, gruppe2,
-                movie3, price3, private3, gruppe3
+    private Component getCard(String movieTitle, double moviePrice, OptionalDouble popcornFree){
+        Paragraph free;
 
-        );
+        H2 movie = new H2(movieTitle);
+        Paragraph price = new Paragraph(moviePrice + " Euro");
+        if (popcornFree.isPresent()){
+                free = new Paragraph("Gratis Popcorn bei Bestellung Gesamtbetrag über " + popcornFree.getAsDouble() + " Euro.");
+        } else {
+                free = new Paragraph("Kein Gratis Popcorn.");
+        }
+        VerticalLayout card = new VerticalLayout(movie, price, free);
+        card.setWidth("350px");
+        card.setPadding(true);
+        card.setSpacing(false);
+
+        card.getStyle()
+                .set("border", "1px solid lightgray")
+                .set("border-radius", "10px")
+                .set("margin", "10px");
+        return card;
     }
 
 }
